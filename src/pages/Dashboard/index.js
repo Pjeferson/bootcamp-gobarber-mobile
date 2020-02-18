953a4e403d5677803b0a15ac40027f2baeeedf39
Pjeferson <paulojeferson34@gmail.com>
2020-02-18 16:31:31 -0300

    Editing default navigation tab infos

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import Background from '~/components/Background';
 
 import { Container } from './styles';
 
 export default function Dashboard() {
-  return <Container />;
+  return (
+    <Background>
+      <Container />
+    </Background>
+  );
 }
+
+Dashboard.navigationOptions = {
+  tabBarLabel: 'Agendamentos',
+  tabBarIcon: ({ tintColor }) => (
+    <Icon name="event" size={20} color={tintColor} />
+  ),
+};
