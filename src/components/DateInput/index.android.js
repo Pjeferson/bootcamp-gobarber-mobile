7156121fae2c9ab80832c2550644ca1bfdaa7375
Pjeferson <paulojeferson34@gmail.com>
2020-02-19 16:41:36 -0300

    Implementing a DateInput component based on os

---
+++ src/components/DateInput/index.android.js
@@ -0,0 +1,47 @@
+import React, { useMemo } from 'react';
+import { DatePickerAndroid, Alert } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { format } from 'date-fns';
+import pt from 'date-fns/locale/pt';
+
+import { differenceInDays } from 'date-fns/esm';
+import { Container, DateButton, DateText } from './styles';
+
+export default function DateInput({ date, onChange }) {
+  const dateFormatted = useMemo(
+    () =>
+      format(date, "dd 'de' MMMM 'de' yyyy", {
+        locale: pt,
+      }),
+    [date]
+  );
+
+  async function handleOpenPicker() {
+    const { action, year, month, day } = await DatePickerAndroid.open({
+      mode: 'spinner',
+      date,
+    });
+
+    if (action === DatePickerAndroid.dateSetAction) {
+      const selectedDate = new Date(year, month, day);
+
+      if (differenceInDays(selectedDate, new Date()) >= 0) {
+        onChange(selectedDate);
+      } else {
+        Alert.alert(
+          'Erro na escolha de data',
+          'Datas expiradas não podem ser escolhidas.'
+        );
+      }
+    }
+  }
+
+  return (
+    <Container>
+      <DateButton onPress={handleOpenPicker}>
+        <Icon name="event" size={20} color="#fff" />
+        <DateText>{dateFormatted}</DateText>
+      </DateButton>
+    </Container>
+  );
+}
