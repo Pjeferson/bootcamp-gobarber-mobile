1e3071d561f3b1f587511be8ba9b8fe65b539406
Pjeferson <paulojeferson34@gmail.com>
2020-02-19 15:46:39 -0300

    Getting providers list from API

--- src/pages/New/SelectProvider/index.js
+++ src/pages/New/SelectProvider/index.js
@@ -1,15 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { TouchableOpacity } from 'react-native';
+import PropTypes from 'prop-types';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Background from '~/components/Background';
 
-import { Container } from './styles';
+import { Container, ProvidersList, Provider, Avatar, Name } from './styles';
+import api from '~/services/api';
 
-export default function SelectProvider() {
+export default function SelectProvider({ navigation }) {
+  const [providers, setProviders] = useState([]);
+
+  useEffect(() => {
+    async function loadProviders() {
+      const response = await api.get('/providers');
+
+      setProviders(response.data);
+    }
+    loadProviders();
+  }, []);
+
   return (
     <Background>
-      <Container />
+      <Container>
+        <ProvidersList
+          data={providers}
+          keyExtractor={provider => String(provider.id)}
+          renderItem={({ item: provider }) => (
+            <Provider
+              onPress={() => {
+                navigation.navigate('SelectDateTime', { provider });
+              }}
+            >
+              <Avatar
+                source={{ uri: provider.avatar && provider.avatar.url }}
+              />
+              <Name>{provider.name}</Name>
+            </Provider>
+          )}
+        />
+      </Container>
     </Background>
   );
 }
@@ -26,3 +56,9 @@ SelectProvider.navigationOptions = ({ navigation }) => ({
     </TouchableOpacity>
   ),
 });
+
+SelectProvider.propTypes = {
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func,
+  }).isRequired,
+};
