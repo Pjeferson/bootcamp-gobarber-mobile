b551d382196863baebf77d2e5c10bf0334223820
Pjeferson <paulojeferson34@gmail.com>
2020-02-19 20:59:30 -0300

    Implementing Hours list and selection functionality

--- src/pages/New/SelectDateTime/index.js
+++ src/pages/New/SelectDateTime/index.js
@@ -1,20 +1,55 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { TouchableOpacity } from 'react-native';
 import PropTypes from 'prop-types';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
+import api from '~/services/api';
+
 import Background from '~/components/Background';
 import DateInput from '~/components/DateInput';
 
-import { Container } from './styles';
+import { Container, HoursList, Hour, Time } from './styles';
 
-export default function SelectDateTime() {
+export default function SelectDateTime({ navigation }) {
   const [date, setDate] = useState(new Date());
+  const [hours, setHours] = useState([]);
+
+  const provider = navigation.getParam('provider');
+
+  useEffect(() => {
+    async function loadSchedule() {
+      const response = await api.get(`/providers/${provider.id}/available`, {
+        params: {
+          date: date.getTime(),
+        },
+      });
+
+      setHours(response.data);
+    }
+    loadSchedule();
+  }, [date, provider.id]);
+
+  function handleSelectTime(time) {
+    navigation.navigate('Confirm', { provider, time });
+  }
 
   return (
     <Background>
       <Container>
         <DateInput date={date} onChange={setDate} />
+
+        <HoursList
+          data={hours}
+          keyExtractor={hour => hour.time}
+          renderItem={({ item: hour }) => (
+            <Hour
+              onPress={() => handleSelectTime(hour.value)}
+              enabled={hour.available}
+            >
+              <Time>{hour.time}</Time>
+            </Hour>
+          )}
+        />
       </Container>
     </Background>
   );
@@ -32,3 +67,10 @@ SelectDateTime.navigationOptions = ({ navigation }) => ({
     </TouchableOpacity>
   ),
 });
+
+SelectDateTime.propTypes = {
+  navigation: PropTypes.shape({
+    getParam: PropTypes.func,
+    navigate: PropTypes.func,
+  }).isRequired,
+};
