4965d36dbc6ab6eee8675972c0b3197d72927983
Pjeferson <paulojeferson34@gmail.com>
2020-02-19 10:50:51 -0300

    Getting appointments from API

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,21 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Background from '~/components/Background';
 import Appointment from '~/components/Appointment';
 
 import { Container, Title, List } from './styles';
-
-const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+import api from '~/services/api';
 
 export default function Dashboard() {
+  const [appointments, setAppointments] = useState([]);
+  useEffect(() => {
+    async function loadAppointments() {
+      const response = await api.get('/appointments');
+
+      const data = response.data.map(a => a);
+
+      setAppointments(data);
+    }
+
+    loadAppointments();
+  }, []);
+
   return (
     <Background>
       <Container>
         <Title>Agendamentos</Title>
         <List
-          data={data}
-          keyExtractor={item => String(item)}
+          data={appointments}
+          keyExtractor={item => String(item.id)}
           renderItem={({ item }) => <Appointment data={item} />}
         />
       </Container>
